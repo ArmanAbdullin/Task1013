a568a9796d4930a9161ba03490d65288fa434e31
2a.k <argin9599@gmail.com>
2019-03-02 21:09:13 +0400

    Task1013

---
+++ Task1013.java
@@ -0,0 +1,102 @@
+// Напиши класс Human с 6 полями.
+// Придумай и реализуй 10 различных конструкторов для него.
+// Каждый конструктор должен иметь смысл.
+
+// Требования:
+// Программа не должна считывать данные с клавиатуры.
+// В классе Human должно быть 6 полей.
+// Все поля класса Human должны быть private.
+// В классе Human должно быть 10 конструкторов.
+// Все конструкторы класса Human должны быть public
+
+public class Task1013{
+	 public static void main(String[] args)
+    {
+
+    }
+
+    public static class Human
+    {
+        private String name;
+        private String surname;
+        private int age;
+        private String address;
+        private int phone;
+        private String car;
+
+        public Human(String name)
+        {
+            this.name = name;
+        }
+
+        public Human(String name, String surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public Human(String name, String surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public Human(String name, String surname, int age, String address)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Human(String name, String surname, int age, String address, int phone)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        public Human(String name, String surname, int age, String address, int phone, String car)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.address = address;
+            this.phone = phone;
+            this.car = car;
+        }
+
+        public Human(String surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public Human(String surname, int age, String address)
+        {
+            this.surname = surname;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Human(String surname, int age, String address, int phone)
+        {
+            this.surname = surname;
+            this.age = age;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        public Human(String surname, int age, String address, int phone, String car)
+        {
+            this.surname = surname;
+            this.age = age;
+            this.address = address;
+            this.phone = phone;
+            this.car = car;
+        }
+    }
+}
